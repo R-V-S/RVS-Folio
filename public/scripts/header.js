a4b3031f15173caa83035788ee1adf35e623b03e
Raj <rajeev.v.singh@gmail.com>
2016-04-22 08:57:23 -0400

    adding a fade-in to the header after height adjustment

--- public/scripts/header.js
+++ public/scripts/header.js
@@ -2,8 +2,15 @@ $(document).ready(function() {
   var $header = $('#header');
   var $headerContents = $('#header-contents');
   var minWidth = 600;
+  var loaded = false;
+
   var resizeHeader = function() {
     $header.height( $(window).height() );
+
+    if (!loaded) {
+      loaded = true;
+      setTimeout( function() { $header.addClass('loaded'); }, 380);
+    }
 
     if ( $(window).width() < minWidth) {
       var newScale = $(window).width() / minWidth;
